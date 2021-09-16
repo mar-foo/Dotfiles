934ae7961821de59c64788da7f41727ff19a7c59
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2021-09-16 22:02:43 +0200

    dwm: Remove unused scripts from /* commands */ section

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -56,16 +56,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *browser[]  = {"surf-open.sh", NULL};
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *lock[]     = {"slock", NULL};
-static const char *lowervol[] = {"amixer", "sset", "Master", "5%-", NULL};
-static const char *mixer[]    = {"st", "-e", "alsamixer", NULL};
-static const char *music[]    = {"cmus_open.sh", NULL};
-static const char *nnn[]      = {"st", "-e", "nnn", NULL};
-static const char *raisevol[] = {"amixer", "sset", "Master", "5%+", NULL};
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL};
 static const char *termcmd[]  = { "st", NULL };
-static const char *unicode[]  = {"dmenuunicode", NULL};
+static const char *emacscmd[] = { "em-daemon", NULL };
+static const char *lowervol[] = { "amixer", "sset", "Master", "5%-", NULL};
+static const char *raisevol[] = { "amixer", "sset", "Master", "5%+", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
